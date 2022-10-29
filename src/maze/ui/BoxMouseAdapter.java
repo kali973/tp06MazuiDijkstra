ed4f2d9296d6d444b35f1f54b577db52849a8776
coutard <gilles.coutard@telecom-paris.fr>
2022-10-29 04:58:20 -0400

    TP06 TP Projet Labyrinthe Telecom Paris Initialisation

--- src/maze/ui/BoxMouseAdapter.java
+++ src/maze/ui/BoxMouseAdapter.java
@@ -4,7 +4,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class BoxMouseAdapter extends MouseAdapter {
-
     private BoxPanel boxPanel;
 
     public BoxMouseAdapter(BoxPanel boxPanel) {
