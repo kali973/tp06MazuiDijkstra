c997a4b2e9a56960863740784d8af3945c25630b
coutard <gilles.coutard@telecom-paris.fr>
2022-10-29 04:56:38 -0400

    TP06 TP Projet Labyrinthe Telecom Paris Initialisation

--- src/maze/ui/ArrivalBoxButton.java
+++ src/maze/ui/ArrivalBoxButton.java
@@ -14,7 +14,6 @@ import java.awt.event.ActionListener;
  */
 @SuppressWarnings("serial")
 public class ArrivalBoxButton extends AbstractBoxTypeRadioButton implements ActionListener {
-
     public ArrivalBoxButton(final MazePanel mazePanel) {
         super("Arrival Box", mazePanel);
     }
