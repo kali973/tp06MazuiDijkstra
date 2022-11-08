ba8f9b06cad0ef25abeb8b1d6e384d4f4162f881
coutard <gilles.coutard@telecom-paris.fr>
2022-11-08 12:32:11 -0500

    TP6 Refactoring package

--- src/main/java/com/example/telecom/paris/maze/services/Labyrinthe.java
+++ src/main/java/com/example/telecom/paris/maze/services/Labyrinthe.java
@@ -4,7 +4,6 @@ package com.example.telecom.paris.maze.services;
 import com.example.telecom.paris.maze.model.MazeModel;
 import com.example.telecom.paris.maze.ui.MazeEditor;
 
-
 public class Labyrinthe {
     public static void main(String[] args) {
 
